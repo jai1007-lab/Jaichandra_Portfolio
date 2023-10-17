051beb04f082a192e89fb62e33ea4a35fbdf8a8b
Jaichandra Jadhav <74004879+jai1007-lab@users.noreply.github.com>
2023-10-17 16:49:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import React from 'react';
-import './Home.css'; // Import the CSS file
-// import '../Header/Header.js'
-import Projects from '../Projects/Projects'
-import About from '../About/About'
+import Header from './components/Header/Header';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact.js';
 
-import Contact from '../Contact/Contact';
-
-// const projects = [
-//   {
-//     title: 'Project 1',
-//     description: 'Description of Project 1.',
-//   },
-//   {
-//     title: 'Project 2',
-//     description: 'Description of Project 2.',
-//   },
-//   // Add more projects as needed
-// ];
-
-const Home = () => {
+function App() {
   return (
-    <div> 
 
-    <About />
+      <div>
+      <Header/>
+      <About />
+      <Projects />
+      <Contact />
+      </div>
 
-    <Projects />
-
-    <hr></hr>
-
-    <Contact />
-    </div>
   );
-};
+}
 
-export default Home;
+export default App;
