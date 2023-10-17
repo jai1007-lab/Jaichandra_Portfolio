bb535304ec7913838546ce53a32c09cb5024513b
Jai_0710 <74004879+jai1007-lab@users.noreply.github.com>
2023-10-17 15:12:58 -0400

    Updates

---
+++ precache-manifest.fa2c732d9786eddbd1574e215f86c3d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9306d6fd6c9aafd20e821a8eb5106804",
+    "url": "/Jaichandra_Portfolio/index.html"
+  },
+  {
+    "revision": "d6429980fe627450ddcf",
+    "url": "/Jaichandra_Portfolio/static/css/main.976e813a.chunk.css"
+  },
+  {
+    "revision": "fc53cc0959b8dbedfce8",
+    "url": "/Jaichandra_Portfolio/static/js/2.e2c4158e.chunk.js"
+  },
+  {
+    "revision": "52b9eb7b02795767ae23",
+    "url": "/Jaichandra_Portfolio/static/js/3.98ddad6b.chunk.js"
+  },
+  {
+    "revision": "d6429980fe627450ddcf",
+    "url": "/Jaichandra_Portfolio/static/js/main.14d6f8c4.chunk.js"
+  },
+  {
+    "revision": "28c8d453b48f0dd7e45b",
+    "url": "/Jaichandra_Portfolio/static/js/runtime~main.20caebc8.js"
+  }
+]);
